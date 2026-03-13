ffc4f9c8f35e87c7016f0e991c49636d7bb1b0f3
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:41:48 -0500

    decode again

--- src/DecodeStrings.java
+++ src/DecodeStrings.java
@@ -11,9 +11,19 @@ okay wat
 
 so i see my number k and im going to wait
 im confused i skip over
+umm
 
+okay so i have a compressed string and im trying to uncompress it like a zip file ? where its lossy conversion type of thing
 
+so my compressed part is repeated k times which im given
+
+so when i see like a number and then whatever is in the brackets im supposed to just make it that many times
+but if i have those brcakts next to each other then i need my stack to keep track of where i am liek the last one okay
+
+so im going to make my stack and copy from the other one if you see an open put it in until you find the close one then pop open out
+which would workf or if the thigns are nested still
      */
+
 
 
 
