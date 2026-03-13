7105a8344375b1865858adab3aef8702bae756e4
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:23:21 -0500

    BalancedSymbols

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
 public class main {
+
+
+    public static void main(String[] args) {
+
+        BalancedSymbols balancedSymbols = new BalancedSymbols();
+        System.out.println(balancedSymbols.isBalanced("<>(){}"));
+        System.out.println(balancedSymbols.isBalanced("<>(>){}"));
+    }
 }
