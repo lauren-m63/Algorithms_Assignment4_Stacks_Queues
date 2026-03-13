c85c0da55599826b6c8d06a7b1628e4ba59025c0
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:22:12 -0500

    BalancedSymbols

--- src/BalancedSymbols.java
+++ src/BalancedSymbols.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class BalancedSymbols {
 
 /*
@@ -8,13 +10,41 @@ or i have to use a stack so
 im going to go through the string one char at a time so ill convert it to a char array
 then if its an opening one ill push onto stack and if its a closing then ill popit if not then ill continue checking them
 chill
+
+then time complexity is just O(n) because I go through each thing just once - elemnt in char aray which is length of string
  */
 
     public static String isBalanced(String s) {
 
+        LinkedList<Character> stack = new LinkedList<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(' || c == '<' || c == '¿') {
+                stack.push(c);
+            }
+            else{
+                if (stack.isEmpty()) return "NO";
+            }
 
-        return "Yes";
+            char top = stack.peek();
+
+            if ((c == ')' && top == '(') ||
+                    (c == ']' && top == '[') ||
+                    (c == '}' && top == '{') ||
+                    (c == '>' && top == '<') ||
+                    (c == '?' && top == '¿')) {
+                stack.pop();
+            }// end if
+            else{
+                return "NO";
+            }
+
+        }// end for loop
+
+    if (stack.isEmpty()) return "YES";
+
+        return "NO";
     }
 
 }
