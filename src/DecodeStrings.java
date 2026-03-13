07b484efd97fe10266cbe4a6b66e36cdea4d179d
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:31:25 -0500

    DecodeStrings

--- src/DecodeStrings.java
+++ src/DecodeStrings.java
@@ -1,2 +1,18 @@
 public class DecodeStrings {
-}
+
+    /*
+
+    okay so
+    you have a string and u want to decode it
+    i dont have to do any wellness checks on the inptu
+    im given how many times the encoded string is repeated and then also the string itself so
+
+
+     */
+
+
+
+
+
+
+} //LAST BRACKET
