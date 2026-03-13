d09cf627470a8c914373386548de08ffdc3d67f3
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:55:55 -0500

    decode fix actual

--- src/main.java
+++ src/main.java
@@ -8,7 +8,12 @@ public class main {
         System.out.println(balancedSymbols.isBalanced("<>(>){}")); //no
 
 
-        System.out.println(DecodeStrings.decode("a+b*(c^d-e)^(f+g*h)-i")); //abcd^e-fgh*+^*+i-
-        // im pretty sure my output is fine- its not itr wrong
+        //System.out.println(DecodeStrings.decode("a+b*(c^d-e)^(f+g*h)-i")); //abcd^e-fgh*+^*+i-
+        // im pretty sure my output is fine- its not itr wrong durrr
+        // ohhh this is for post fix is why its won okay
+
+        System.out.println(DecodeStrings.decode("3[a]2[bc]"));
+        System.out.println(DecodeStrings.decode("3[a2[c]]"));
+        System.out.println(DecodeStrings.decode("2[abc]3[cd]ef"));// okayyyyyy
     }
 }
