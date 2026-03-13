17187b5cf4f258be3fde0993a4f8b4232fab84ee
lauren-m63 <lauren.m63@yahoo.com>
2026-03-12 21:38:46 -0500

    InfixToPostfix

--- src/DecodeStrings.java
+++ src/DecodeStrings.java
@@ -7,6 +7,11 @@ public class DecodeStrings {
     i dont have to do any wellness checks on the inptu
     im given how many times the encoded string is repeated and then also the string itself so
 
+okay wat
+
+so i see my number k and im going to wait
+im confused i skip over
+
 
      */
 
--- src/InfixToPostfix.java
+++ src/InfixToPostfix.java
@@ -1,2 +1,23 @@
 public class InfixToPostfix {
-}
+
+/*
+
+so i should do the matches again
+if i has in then i return the out
+i dont htink i have to do the other way around
+
+if its ( push to th estack and if ) then pop until you get an open ( to know when you are done to keep track
+i hate this one too im going back to the other one
+
+ */
+
+
+    public static String InfixtoPost(String s){
+
+
+
+        return "";
+    } // END METHOD
+
+
+} //END CLASS LAST BRACKET
